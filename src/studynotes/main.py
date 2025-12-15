3c7e6ced119608b8e2e67240c04b8cef93be95c5
ltdat07 <danielle172004@gmail.com>
2025-12-15 19:49:51 +0300

    P11: force no-cache headers

--- src/studynotes/main.py
+++ src/studynotes/main.py
@@ -117,7 +117,9 @@ async def attach_correlation_id(request: Request, call_next):
         },
     )
 
-    response.headers["Cache-Control"] = "no-store"
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, private"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
     return response
 
 
